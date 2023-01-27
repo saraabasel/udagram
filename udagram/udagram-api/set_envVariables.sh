756959a57d8854f5ec57b67a2fd8de67de555d71
saraabasel <sarah.basel94@gmail.com>
2023-01-28 01:10:45 +0200

    Deploying backend

--- udagram/udagram-api/set_envVariables.sh
+++
@@ -1,1 +0,0 @@
-eb setenv POSTGRES_HOST=database-1.cy8qz5z5q2cj.us-east-1.rds.amazonaws.com POSTGRES_DB=postgres POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=postgres AWS_REGION=us-east-1 AWS_PROFILE=default AWS_BUCKET=mediabucket123123 URL=http://udagramapi-dev.us-east-1.elasticbeanstalk.com/ JWT_SECRET=JSDHJSHKSA
